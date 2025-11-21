9c9e58b802f83c66f5239c72e1e5af2c550598ab
AarshAlpPatel <aarsh.patel@gmail.com>
2025-11-21 15:43:31 -0500

    first commit

---
+++ api/api.py
@@ -0,0 +1,12 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
+@app.route('/api/hello')
+def say_hello_back():
+    return {'hello-world': 'Jai Swaminarayan'}
